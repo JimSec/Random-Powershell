e1069a2f948861d2d25761e4bec38894a2370735
JimSec <32644377+JimSec@users.noreply.github.com>
2019-04-26 11:42:33 -0500

    Added Additional Comments

--- Generate-SimplePassword.ps1
+++ Generate-SimplePassword.ps1
@@ -22,11 +22,14 @@
          while ($NewPassword.Length -le $Password_Length) {
 
         
-                #Random Seed to pick which char array a random item will be pulled from.
+            #Random Seed to pick which char array a random item will be pulled from.
 
             $char_type_seed = Get-Random -Maximum 61
 
             switch ( $char_type_seed ) {
+
+
+                #61 Total Characters so ranges have been updated to make sure someone doesn't get a password of all majority Symbols.
 
 
                 {0..23 -contains $char_type_seed} {$char_type = $Uppers}
@@ -48,11 +51,17 @@
             }
     
 
-    } Until(($null -ne ($Uppers | ? { $Script:NewPassword -cmatch $_ }))`
+    } 
+    
+    #Checks that $Script:NewPassword Contains at least one character from each array,or regenerates until it does.
+
+    Until(($null -ne ($Uppers | ? { $Script:NewPassword -cmatch $_ }))`
      -and ($null -ne ($Lowers | ? { $Script:NewPassword -cmatch $_ }))`
      -and ($null -ne ($Numbers | ? { $Script:NewPassword -cmatch $_ }))`
      -and ($null -ne ($Symbols | ? { $Script:NewPassword -cmatch [Regex]::Escape($_) })) )
 
+
+    #SecureString Switch, nothing crazy I just got sick of converting these elsewhere in other scripts. 
     if ($SecureString) {
 
         $Script:NewPassword = $Script:NewPassword | ConvertTo-SecureString -AsPlainText -Force
